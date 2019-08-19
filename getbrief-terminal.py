fba993771dedd36f9a4a548e440794f491fc913a
Sujan Guditi <30351372+gsujan1@users.noreply.github.com>
2019-08-19 12:32:28 -0400

    Update getbrief-terminal.py

--- getbrief-terminal.py
+++ getbrief-terminal.py
@@ -3,30 +3,32 @@ from bs4 import SoupStrainer
 import requests
 from PIL import Image
 
-#load in url
+#load in url and parse necessary information from wikipedia
 url = 'https://en.wikipedia.org/wiki/Main_Page'
 source = requests.get(url)
 
-# loads in necessary html blocks
+# only_mp_upper = SoupStrainer(id='mp-upper')
 only_featured_article = SoupStrainer(id='mp-tfa')
 only_did_you_know = SoupStrainer(id='mp-dyk')
 only_news = SoupStrainer(id='mp-itn')
-# # uncomment line below to get special events on today's date
+# uncomment to get special events on today's date
 # only_on_this_day = SoupStrainer(id='mp-otd')
 only_featured_pic = SoupStrainer(id='mp-lower')
 
-# parse necessary information from Wikipedia's main page
 soup_featured_article = BeautifulSoup(source.content, 'lxml', parse_only = only_featured_article)
 soup_did_you_know = BeautifulSoup(source.content, 'lxml', parse_only = only_did_you_know)
 soup_news = BeautifulSoup(source.content, 'lxml', parse_only = only_news)
-# # uncomment line below to get special events on today's date
+# uncomment to get special events on today's date
 # soup_on_this_day = BeautifulSoup(source.content, 'lxml', parse_only = only_on_this_day)
 soup_featured_pic = BeautifulSoup(source.content, 'lxml', parse_only = only_featured_pic)
 
-######################### FEATURED ARTICLE ######################################
+######################### FEATURED ARTICLE ################################
 featured_article = ''
 
-for string in soup_featured_article.stripped_strings:
+list_featured_article = iter(soup_featured_article.stripped_strings)
+next(list_featured_article)
+
+for string in list_featured_article:
     featured_article = featured_article + string + ' '
 
 splitter_featured_article = '( Full'
@@ -44,6 +46,8 @@ did_you_know = ''
 
 list_did_you_know = iter(soup_did_you_know.stripped_strings)
 next(list_did_you_know)
+next(list_did_you_know)
+
 for string in list_did_you_know:
     did_you_know = did_you_know + string + ' '
 
@@ -56,12 +60,13 @@ did_you_know = did_you_know.replace('... ', '')
 did_you_know = did_you_know.replace(' ... ', '\n')
 did_you_know = did_you_know.replace(' , ', ', ')
 did_you_know = did_you_know.replace('? ', '?\n')
+did_you_know = did_you_know.replace('that', 'THAT')
 
 print('DID YOU KNOW ?')
 print(str(did_you_know))
 print(' ')
 
-######################### IN THE NEWS #######################################
+######################### IN THE NEWS ################################
 in_the_news = ''
 
 list_in_the_news = iter(soup_news.stripped_strings)
@@ -81,10 +86,10 @@ print('IN THE NEWS :')
 print(str(in_the_news))
 print(' ')
 
-################################################################################
+########################################################################
 # I don't need this information, it's not useful/interesting for me
-# but one can uncomment this section to get unformatted text from this section
-# ######################### ON THIS DAY ########################################
+# but one can uncomment this section to get unformatted text
+# ######################### ON THIS DAY ################################
 # on_this_day = ''
 
 # for string in soup_on_this_day.stripped_strings:
@@ -97,7 +102,7 @@ print(' ')
 # print(str(on_this_day))
 # print(' ')
 
-########################## TODAY'S PICTURE ######################################
+########################## TODAY'S PICTURE ###############################
 # prints text for featured text
 featured_pic = ''
 for string in soup_featured_pic.stripped_strings:
@@ -125,8 +130,8 @@ link = 'https:' + link
 
 img_url = str(link)
 img_request = requests.get(img_url)
-open('current_wiki_img', 'wb').write(img_request.content)
+open('current_wiki_img.png', 'wb').write(img_request.content)
 
-img = Image.open('current_wiki_img')
+img = Image.open('current_wiki_img.png')
 img = img.resize((550,450))
 img.show()
